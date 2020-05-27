0024e7dd587b20145b084ec587a6751a95608ca1
gy0851 <geyao_0851@163.com>
2020-05-27 14:23:17 +0800

    更新了一个验证码的demo

--- MySpringTestDemo/src/main/java/Main.java
+++ MySpringTestDemo/src/main/java/Main.java
@@ -1,7 +1,5 @@
 import com.ggyy0851.pojo.Student;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
